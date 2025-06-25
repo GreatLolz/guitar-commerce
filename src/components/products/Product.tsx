225bf4e755f782db73b20724a189280be81abd95
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-06-26 01:06:00 +0200

    add props to Product component

--- src/components/products/Product.tsx
+++ src/components/products/Product.tsx
@@ -1,11 +1,11 @@
 import { Star } from "lucide-react";
 
-export default function Product() {
+export default function Product({ imageUrl, name, price, rating, reviewCount }: { imageUrl: string, name: string, price: number, rating: number, reviewCount: number }) {
     return (
         <div className="bg-neutral-50 p-10 flex flex-col items-center hover:shadow-md hover:cursor-pointer gap-2 w-70 h-100">
-            <img src="https://picsum.photos/128" alt="guitar" className="w-50 aspect-square" />
-            <span className="w-full wrap-break-word line-clamp-2">Pretty Long Product Name It's Actually Pretty Longe It's Actually Pretty Long</span>
-            <span className="w-full text-xl font-bold">$799.00</span>
+            <img src={imageUrl} alt={name} className="w-50 aspect-square" />
+            <span className="w-full wrap-break-word line-clamp-2">{name}</span>
+            <span className="w-full text-xl font-bold">${price}</span>
             <div className="w-full flex relative">
                 <div className="flex flex-row w-full">
                     <Star className="text-neutral-500" strokeWidth={1} size={20}/>
@@ -14,11 +14,11 @@ export default function Product() {
                     <Star className="text-neutral-500" strokeWidth={1} size={20}/>
                     <Star className="text-neutral-500" strokeWidth={1} size={20}/>
                     <div className="flex flex-row w-full absolute top-0 left-0">
-                        <Star className="text-yellow-500" strokeWidth={2} fill="oklch(79.5% 0.184 86.047)" size={20}/>
-                        <Star className="text-yellow-500" strokeWidth={2} fill="oklch(79.5% 0.184 86.047)" size={20}/>
-                        <Star className="text-yellow-500" strokeWidth={2} fill="oklch(79.5% 0.184 86.047)" size={20}/>
+                        {Array.from({ length: rating }, (_, index) => (
+                            <Star key={index} className="text-yellow-500" strokeWidth={2} fill="oklch(79.5% 0.184 86.047)" size={20}/>
+                        ))}
                     </div>
-                    <span className="text-sm ml-1">(25)</span>
+                    <span className="text-sm ml-1">({reviewCount})</span>
                 </div>
             </div>
         </div>
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,12 +1,42 @@
 import Product from "../components/products/Product";
 
+const products = [
+    {
+        imageUrl: "https://picsum.photos/128",
+        name: "Epiphone Les Paul Custom Widow Electric Guitar Indigo Burst",
+        price: 799.00,
+        rating: 4,
+        reviewCount: 25
+    },
+    {
+        imageUrl: "https://picsum.photos/128",
+        name: "Jackson Dinky JS22 DKA Arch Top Electric Guitar Satin Black",
+        price: 249.99,
+        rating: 5,
+        reviewCount: 88
+    },
+    {
+        imageUrl: "https://picsum.photos/128",
+        name: "Squier Sonic Stratocaster HSS Maple Fingerboard Electric Guitar Black",
+        price: 230.99,
+        rating: 4,
+        reviewCount: 12
+    },
+    {
+        imageUrl: "https://picsum.photos/128",
+        name: "Squier Classic Vibe '50s Stratocaster Electric Guitar Shell Pink",
+        price: 449.99,
+        rating: 3,
+        reviewCount: 5
+    }
+]
+
 export default function Products() {
     return (
         <div className="grid grid-cols-4 gap-2 mx-auto py-10">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            {products.map((product, index) => (
+                <Product key={index} imageUrl={product.imageUrl} name={product.name} price={product.price} rating={product.rating} reviewCount={product.reviewCount}/>
+            ))}
         </div>
     )
 }
