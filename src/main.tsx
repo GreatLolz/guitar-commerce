7bc57eaf962587d9be62d7e263a84402cbd6ddc0
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-09-01 16:15:02 +0200

    install react redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router'
 import { SidebarProvider } from './context/SidebarContext'
+import { Provider } from 'react-redux'
+import { store } from './store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
-      <SidebarProvider>
-        <App />
-      </SidebarProvider>
+      <Provider store={store}>
+        <SidebarProvider>
+          <App />
+        </SidebarProvider>
+      </Provider>
     </StrictMode>
   </BrowserRouter>
 )
