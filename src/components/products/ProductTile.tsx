84b24c7f558344caf72f99a3f17a9581f7606ef3
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-09-02 17:01:53 +0200

    fix ProductTile not being fully clickable

--- src/components/products/ProductTile.tsx
+++ src/components/products/ProductTile.tsx
@@ -12,15 +12,16 @@ interface ProductTileProps {
 
 export default function ProductTile({ id, imageUrl, name, price, rating, reviewCount }: ProductTileProps) {
     return (
-        <div className="bg-neutral-50 p-10 flex flex-col items-center hover:shadow-md hover:cursor-pointer gap-2 w-70 h-100">
-            <NavLink to={`/products/${id}`}>
-                <div className="w-full h-50 overflow-hidden">
-                    <img src={imageUrl} alt={name} className="w-full h-full object-contain" />
-                </div>
-                <span className="w-full wrap-break-word line-clamp-2 leading-5 h-10">{name}</span>
-                <span className="w-full text-xl font-bold">${price}</span>
-            </NavLink>
-            <Rating rating={rating} reviewCount={reviewCount}/>
-        </div>
+        <NavLink to={`/products/${id}`}>
+            <div className="bg-neutral-50 p-10 flex flex-col items-center hover:shadow-md hover:cursor-pointer gap-2 w-70 h-100">
+
+                    <div className="w-full h-50 overflow-hidden">
+                        <img src={imageUrl} alt={name} className="w-full h-full object-contain" />
+                    </div>
+                    <span className="w-full wrap-break-word line-clamp-2 leading-5 h-10">{name}</span>
+                    <span className="w-full text-xl font-bold">${price}</span>
+                <Rating rating={rating} reviewCount={reviewCount}/>
+            </div>
+        </NavLink>
     )
 }
