c71174efb3bad1cd82d34602424a293d4d06b48b
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-09-10 16:38:29 +0200

    show cart item prices, total cost and some styling fixes

--- src/components/sidebar/CartBar.tsx
+++ src/components/sidebar/CartBar.tsx
@@ -10,6 +10,10 @@ export default function CartBar() {
 
     return (
         <div className="p-5 flex flex-col gap-2">
+            <div className="flex flex-row gap-2 items-center">
+                <p className="text-xl">Total:</p>
+                <p className="text-xl font-bold">${cart.items.reduce((total, item) => total + item.product.price * item.quantity, 0).toFixed(2)}</p>
+            </div>
             {cart.items.map((item, index) => (
                 <div key={index} className="flex flex-row gap-2 items-center justify-between bg-white px-5 p-3 hover:shadow-md">
                     <NavLink 
@@ -17,9 +21,12 @@ export default function CartBar() {
                         className="flex flex-row gap-3 items-center"
                         onClick={() => closeSidebar()}
                     >
-                        <p className="w-full max-w-5">{item.quantity}</p>
+                        <p className="w-full max-w-6 text-xl">{item.quantity}</p>
                         <img src={item.product.imageUrl} alt={item.product.name} className="w-25 h-25 object-contain"/>
-                        <p className="font-semibold text-ellipsis line-clamp-2">{item.product.name}</p>
+                        <div>
+                            <p className="text-ellipsis line-clamp-1">{item.product.name}</p>
+                            <p className="font-semibold text-md w-full text-orange-600">${(item.product.price * item.quantity).toFixed(2)}</p>
+                        </div>
                     </NavLink>
                     <Button onClick={() => removeCartItem(item.id)} className="bg-neutral-400 rounded-full p-0.5 hover:cursor-pointer hover:bg-orange-600">
                         <X size={10} className="text-white"/>
