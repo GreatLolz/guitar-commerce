239ad8a4aad84d42ad4f3de304732d37efe94a23
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-10-13 13:09:57 +0200

    update cart reducer to support shipping cost

--- src/reducers/cart.ts
+++ src/reducers/cart.ts
@@ -4,11 +4,15 @@ import type { Cart } from "../types/cart";
 export const cartSlice = createSlice({
     name: 'cart',
     initialState: {
-        cart: { items: [] } as Cart
+        cart: { items: [] } as Cart,
+        shippingCost: 0
     },
     reducers: {
         setActiveCart: (state, action) => {
             state.cart = action.payload
+        },
+        setShippingCost: (state, action) => {
+            state.shippingCost = action.payload
         },
         addCartItem: (state, action) => {
             state.cart.items.push(action.payload)
@@ -19,6 +23,6 @@ export const cartSlice = createSlice({
     }
 })
 
-export const { setActiveCart, addCartItem, removeCartItem } = cartSlice.actions
+export const { setActiveCart, addCartItem, removeCartItem, setShippingCost } = cartSlice.actions
 
 export default cartSlice.reducer
