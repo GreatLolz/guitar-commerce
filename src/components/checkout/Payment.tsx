39b51e95afdeffdc8a8089db07b977b9372047d1
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-10-13 13:17:38 +0200

    add delivery methods section to checkout

--- src/components/checkout/Payment.tsx
+++ src/components/checkout/Payment.tsx
@@ -1,9 +1,22 @@
 import { Radio, RadioGroup } from "@headlessui/react"
 import { useState } from "react"
-import { PAYMENT_METHODS } from "../../types/checkout"
+import { DELIVERY_METHODS, PAYMENT_METHODS, type PaymentMethod, type DeliveryMethod } from "../../types/checkout"
+import { useCart } from "../../hooks/useCart"
 
 export default function Payment() {
-    const [paymentMethod, setPaymentMethod] = useState<string | null>(null)
+    const [paymentMethod, setPaymentMethod] = useState<PaymentMethod | null>(null)
+    const [deliveryMethod, setDeliveryMethod] = useState<DeliveryMethod | null>(null)
+    const { setShipping } = useCart()
+
+    const handlePaymentMethodChange = (method: PaymentMethod) => {
+        setPaymentMethod(method)
+    }
+
+    const handleDeliveryMethodChange = (method: DeliveryMethod) => {
+        console.log(method.price)
+        setDeliveryMethod(method)
+        setShipping(method.price)
+    }
 
     return (
         <div className="w-full bg-white ml-10 border-1 border-neutral-300 p-5 h-fit">
@@ -11,11 +24,24 @@ export default function Payment() {
             <hr className="my-3 border-neutral-300"/>
             <h2 className="font-bold text-xl">Payment method</h2>
             <div className="flex gap-5 w-full mt-5">
-                <RadioGroup value={paymentMethod} onChange={setPaymentMethod} className="flex gap-5">
+                <RadioGroup value={paymentMethod} onChange={handlePaymentMethodChange} className="flex gap-5">
                     {PAYMENT_METHODS.map((method) => (
-                        <Radio key={method.id} value={method.id} className="group">
+                        <Radio key={method.id} value={method} className="group">
                             <div className="w-48 h-32 border-1 border-neutral-300 flex items-center justify-center hover:cursor-pointer hover:bg-neutral-200 group-data-checked:border-2 group-data-checked:border-orange-500">
-                                <img src={method.icon} alt={method.name} className="w-20 min-w-20" />
+                                {method.icon ? <img src={method.icon} alt={method.name} className="w-20 min-w-20" /> : <p>{method.name}</p>}
+                            </div>
+                        </Radio>
+                    ))}
+                </RadioGroup>
+            </div>
+            <hr className="my-3 mt-5 border-neutral-300"/>
+            <h2 className="font-bold text-xl">Delivery method</h2>
+            <div className="flex gap-5 w-full mt-5">
+                <RadioGroup value={deliveryMethod} onChange={handleDeliveryMethodChange} className="flex gap-5">
+                    {DELIVERY_METHODS.map((method) => (
+                        <Radio key={method.id} value={method} className="group">
+                            <div className="w-48 h-32 border-1 border-neutral-300 flex items-center justify-center hover:cursor-pointer hover:bg-neutral-200 group-data-checked:border-2 group-data-checked:border-orange-500">
+                                {method.icon ? <img src={method.icon} alt={method.name} className="w-20 min-w-20" /> : <p>{method.name}</p>}
                             </div>
                         </Radio>
                     ))}
