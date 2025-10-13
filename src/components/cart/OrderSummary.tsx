e1daaa3f2ead607326398fe82d8f8c82d1ef8e8a
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-10-13 13:17:30 +0200

    use shipping cost in order summary

--- src/components/cart/OrderSummary.tsx
+++ src/components/cart/OrderSummary.tsx
@@ -7,7 +7,7 @@ interface OrderSummaryProps {
 }
 
 export default function OrderSummary({ showButton = true, showCart = true }: OrderSummaryProps) {
-    const { cart } = useCart()
+    const { cart, shippingCost } = useCart()
 
     return (
         <div className="w-full max-w-1/3 bg-white ml-10 border-1 border-neutral-300 p-5 h-fit">
@@ -20,12 +20,12 @@ export default function OrderSummary({ showButton = true, showCart = true }: Ord
                 </div>
                 <div className="flex flex-row justify-between">
                     <p className="text-lg">Shipping</p>
-                    <p className="text-lg font-bold">-</p>
+                    <p className="text-lg font-bold">${shippingCost}</p>
                 </div>
                 <hr className="my-3 border-neutral-300"/>
                 <div className="flex flex-row justify-between">
                     <p className="text-lg font-bold">Estimated Total</p>
-                    <p className="text-lg font-bold">${cart.items.reduce((total, item) => total + item.product.price * item.quantity, 0).toFixed(2)}</p>
+                    <p className="text-lg font-bold">${cart.items.reduce((total, item) => total + shippingCost + item.product.price * item.quantity, 0).toFixed(2) }</p>
                 </div>
                 {showCart && (
                     <div>
